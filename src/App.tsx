f6d12f861d12107f193e8d1a965ca44377c8080c
IndrajeethY <indrajeeth0703@gmail.com>
2025-10-07 10:05:08 +0000

    fix scrollbar routing, make it auto move to page top when navigated to new pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import Index from "./pages/Index";
 import Blogs from "./pages/Blogs";
 import BlogDetail from "./pages/BlogDetail";
@@ -21,6 +22,16 @@ import RuralDevelopment from "./pages/programs/RuralDevelopment";
 import Charity from "./pages/programs/Charity";
 
 const queryClient = new QueryClient();
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "auto" });
+  }, [pathname]);
+
+  return null;
+};
+
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
@@ -28,9 +39,9 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+      <ScrollToTop /> 
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* Program Routes */}
           <Route path="/programs/education" element={<Education />} />
           <Route path="/programs/health" element={<Health />} />
           <Route path="/programs/empowerment" element={<Empowerment />} />
