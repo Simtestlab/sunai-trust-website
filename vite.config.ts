9fff16098a15f3803884551c92192c424455d00f
IndrajeethY <indrajeeth0703@gmail.com>
2025-10-08 07:38:46 +0000

    fix vercel router

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    port: 8080,
+    port: 3000,
     // allow tunnel host for remote previews
     allowedHosts: ["muskox-right-kangaroo.ngrok-free.app"],
   },
